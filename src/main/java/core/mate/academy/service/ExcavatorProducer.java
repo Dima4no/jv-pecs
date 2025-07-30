07002b6b0089381f08a9cd28c5699b942ee491f8
Dima <nds081989@hmail.com>
2025-07-31 00:18:09 +0800

    Fixed extra separation in import classes

--- src/main/java/core/mate/academy/service/ExcavatorProducer.java
+++ src/main/java/core/mate/academy/service/ExcavatorProducer.java
@@ -2,9 +2,8 @@ package core.mate.academy.service;
 
 import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
-
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ExcavatorProducer implements MachineProducer<Machine> {
 
--- src/main/java/core/mate/academy/service/TruckProducer.java
+++ src/main/java/core/mate/academy/service/TruckProducer.java
@@ -2,11 +2,10 @@ package core.mate.academy.service;
 
 import core.mate.academy.model.Machine;
 import core.mate.academy.model.Truck;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class TruckProducer implements MachineProducer<Machine>{
+public class TruckProducer implements MachineProducer<Machine> {
 
     @Override
     public List<Machine> get() {
